29f498f4d8a2a3229befc64db26a9e740190a40a
Andrew <Andrew@andrews-mbp>
2021-01-01 00:12:32 -0500

    - Created loadGamesMenu() - Changed loadMainMenu() to loadHomeMenu() - added some better spacing and dividers

--- Java_Console_SkillShowcase/src/java_console_skillshowcase/Java_Console_SkillShowcase.java
+++ Java_Console_SkillShowcase/src/java_console_skillshowcase/Java_Console_SkillShowcase.java
@@ -14,7 +14,7 @@ public class Java_Console_SkillShowcase
      */
     public static void main(String[] args)
     {
-        loadMainMenu();
+        loadHomeMenu();
     }
 
     
@@ -25,23 +25,25 @@ public class Java_Console_SkillShowcase
     static boolean quit = false;
     
     
-    private static void loadMainMenu()
+    private static void loadHomeMenu()
     {
         while (!quit) {
-            System.out.println("\n\n\n\nWelcome to L. Andrew Overholt's Java Console Skills Showcase!\n");
+            System.out.println("\n\n\n\n**************************************************************\n"
+                             + "Welcome to L. Andrew Overholt's Java Console Skills Showcase!\n"
+                             + "**************************************************************\n\n");
             
             
             // Print the menu
-            c = getChar("** MENU **\n"
-                    + "(G): Games\n"
-                    + "(L): Loops\n"
-                    + "(M): Math\n"
-                    + "(Q): Quit\n\n"
-                    + "Your choice: ");
+            c = getChar("** HOME **\n"
+                      + "(G): Games\n"
+                      + "(L): Loops\n"
+                      + "(M): Math\n"
+                      + "(Q): Quit\n\n"
+                      + "Your choice: ");
             switch (c) {
                 case 'G':
                 case 'g':
-                    System.out.println("\nGames menu has not been created yet");
+                    loadGamesMenu();
                     break;
                 case 'l':
                 case 'L':
@@ -59,6 +61,43 @@ public class Java_Console_SkillShowcase
             }
             
         }
+    }
+    
+    private static void loadGamesMenu() {
+        while (!quit) {
+            System.out.println("\n\n\n\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n"
+                             + " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n\n\n");
+            
+            
+            // Print the menu
+            c = getChar("** GAMES **\n"
+                      + "(M): Mad Lib\n"
+                      + "(G): Guess the Number\n"
+                      + "(H): Home\n"
+                      + "(Q): Quit\n\n"
+                      + "Your choice: ");
+            switch (c) {
+                case 'M':
+                case 'm':
+                    System.out.println("\nMad Lib has not been created yet");
+                    break;
+                case 'G':
+                case 'g':
+                    System.out.println("\nGuess the Number has not been created yet");
+                    break;
+                case 'H':
+                case 'h':
+                    loadHomeMenu();
+                    break;
+                case 'Q':
+                case 'q':
+                    quit = true;
+                    break;
+                default:
+            }
+            
+        }
+        
     }
     
 }
