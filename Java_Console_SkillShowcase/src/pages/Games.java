69e3ae83d2ae4da4b7780189e457517cd5dcb7af
Andrew <Andrew@andrews-mbp>
2021-01-02 13:39:41 -0500

    - added madLib2()

--- Java_Console_SkillShowcase/src/pages/Games.java
+++ Java_Console_SkillShowcase/src/pages/Games.java
@@ -24,7 +24,8 @@ public class Games {
         randNum = rand.nextInt(2); //generate random values from 0-1
         
         switch (randNum) {
-            case 0, 1 -> madLib1();
+            case 0 -> madLib1();
+            case 1 -> madLib2();
             default ->  {
             }
         }
@@ -82,7 +83,10 @@ public class Games {
         
         newPage();
         // print the complete mad lib to the screen
-        System.out.println("Star Wars is a " + adj1 + " " + noun1 + " of " + adj2 + "\n" //line1
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                         + "~~          Star  Wars          ~~\n"
+                         + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                         + "Star Wars is a " + adj1 + " " + noun1 + " of " + adj2 + "\n" //line1
                          + "versus evil in a " + noun2 + " far far away.\n" //line2
                          + "There are " + adj3 + " battles between\n" //line3
                          + adj4 + " " + noun3 + " in " + adj5 +"\n" //line4
@@ -99,11 +103,47 @@ public class Games {
         pressAnyKeyToContinue();
     }
     
+    
     /**
      * madLib2 prompts the user for words and then puts them in the blanks of a 
      * Mad Lib
      */
     public static void madLib2() {
-        // This mad lib hasnt been created yet.
+        
+        newPage();
+
+        // prompt the user for words to fill in the blanks
+        // Line 1
+        String noun1 = getString("Enter a noun: ");
+        String adj1 = getString("Enter an adjective: ");
+        String noun2 = getString("Enter a noun(thing): ");
+        // Line 2
+        String adj2 = getString("Enter an adjective: ");
+        String noun3 = getString("Enter a noun: ");
+        // Line 3
+        String noun4 = getString("Enter a noun(place): ");
+        // Line 4
+        String noun5 = getString("Enter a plural noun: ");
+        String noun6 = getString("Enter a noun(unit of time): ");
+        // Line 5
+        String adv1 = getString("Enter an adverb: ");
+        String verb1 = getString("Enter a verb(past tense): ");
+        // Line 6
+        String adv2 = getString("Enter an adverb: ");
+        
+        
+        newPage();
+        // print the complete mad lib to the screen
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                         + "~~       Pixar's Next Hit       ~~\n"
+                         + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                         + "Once upon a blue " + noun1 + ", a " + adj1 + " " + noun2 + "\n" // line1
+                         + "and a(n) " + adj2 + " dog fell in a(n)" + noun3 + " and it\n" // line2
+                         + "was amazing. They went to " + noun4 + " and ate\n" // line3
+                         + noun5 + " every " + noun6 + ". After some time,\n" // line4
+                         + "they " + adv1 + " parted ways and " + verb1 + "\n" // line5
+                         + adv2 + " every after."); //line6
+        
+        pressAnyKeyToContinue();
     }
 }
