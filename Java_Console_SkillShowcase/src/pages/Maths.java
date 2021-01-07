ea6d5e35e93645693d04d966fd333bb634a8f86d
Andrew <Andrew@andrews-mbp>
2021-01-06 22:24:54 -0500

    - added addition() and subtraction()

--- Java_Console_SkillShowcase/src/pages/Maths.java
+++ Java_Console_SkillShowcase/src/pages/Maths.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import static java_console_skillshowcase.Java_Console_SkillShowcase.newPage;
 import static java_console_skillshowcase.Java_Console_SkillShowcase.pressAnyKeyToContinue;
 import static validation.InputValidation.getChar;
+import static validation.InputValidation.getDouble;
 
 /**
  *
@@ -39,8 +40,8 @@ public class Maths {
             // use the users selection stored in variable c to navigate to the 
             // appropriate screen
             switch (c) {
-                case 'A', 'a' -> System.out.println("\n*** \"Add\" has not been created yet ***");
-                case 'S', 's' -> System.out.println("\n***\"Subtract\" has not been created yet ***");
+                case 'A', 'a' -> addition();
+                case 'S', 's' -> subtraction();
                 case 'M', 'm' -> System.out.println("\n***\"Multiply\" has not been created yet ***");
                 case 'D', 'd' -> System.out.println("\n***\"Divide\" has not been created yet ***");
                 case 'H', 'h' -> java_console_skillshowcase.Java_Console_SkillShowcase.loadHomeMenu();
@@ -56,4 +57,66 @@ public class Maths {
     }
     
     
+    /**
+     * This method allows the user to enter two numbers to be added together
+     */
+    private static void addition() {
+        double num1;
+        double num2;
+        double result;
+        
+        newPage();
+        
+        // print info section
+        System.out.println("~~~~~~~~~~~~~~~~\n"
+                         + ">>  Addition  <<\n"
+                         + "~~~~~~~~~~~~~~~~\n"
+                         + "You will enter two numbers to be added together.\n");
+        
+        // Get the numbers from the user
+        num1 = getDouble("First number: ");
+        num2 = getDouble("Second number: ");
+        
+        // Calculate results
+        result = num1 + num2;
+        
+        // Display results
+        System.out.println("\n" + num1 + " + " + num2 + " = " + result + "\n");
+        
+        // Keep the results up until the user is ready to continue
+        pressAnyKeyToContinue();
+    }
+    
+    
+    /**
+     * This method allows the user to enter two numbers.
+     * The second number will be subtracted from the first.
+     */
+    private static void subtraction() {
+        double num1;
+        double num2;
+        double result;
+        
+        newPage();
+        
+        // print info section
+        System.out.println("~~~~~~~~~~~~~~~~~~~\n"
+                         + ">>  Subtraction  <<\n"
+                         + "~~~~~~~~~~~~~~~~~~~\n"
+                         + "You will enter two numbers.\n"
+                         + "The second number will be subtracted from the first.\n");
+        
+        // Get the numbers from the user
+        num1 = getDouble("First number: ");
+        num2 = getDouble("Second number: ");
+        
+        // Calculate results
+        result = num1 - num2;
+        
+        // Display results
+        System.out.println("\n" + num1 + " + " + num2 + " = " + result + "\n");
+        
+        // Keep the results up until the user is ready to continue
+        pressAnyKeyToContinue();
+    }
 }
